8fa1759cec3d117e172fc739c22e886b826abc5b
MD Waliullah <waliullah9099@gmail.com>
2024-11-15 17:31:39 +0600

    Numerical Operations

---
+++ third.py
@@ -0,0 +1,15 @@
+# variable and data type 
+name = " Habiba Aktar"
+age = 16
+village = "Jethua"
+
+# print(name + "is a good girl. Her age is", age, ". Now she lives in" + village)
+
+# Numerical Operations 
+
+a = 10
+b = 3
+
+
+print(a // b)
+print(a ** b)
